e1497649fe9981cd5ce3d0f5b6b6c5afcaa25a10
evgeniyalexandr0vich <1511jeck@gmail.com>
2025-11-26 15:15:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,12 @@ from datetime import time
 # НАСТРОЙКИ БОТА
 BOT_TOKEN = os.getenv('BOT_TOKEN', '8108841583:AAHNAxCDantgG51JfjyBmDdaubVFWiDHvyI')
 
-# Настройки Excel
-EXCEL_FILE = os.getenv('EXCEL_FILE', 'work_tracker.xlsx')
+# Excel файл в постоянном хранилище
+EXCEL_FILE = os.getenv('EXCEL_FILE', '/data/work_tracker.xlsx')
 
-# Время отправки напоминания по умолчанию (24-часовой формат)
+# Время отправки напоминания по умолчанию
 DEFAULT_REMINDER_HOUR = 18
 DEFAULT_REMINDER_MINUTE = 0
 
-# Словарь для хранения индивидуальных настроек пользователей
 USER_SETTINGS = {}
-
-# Список пользователей, которые уже получили приветственное сообщение
 WELCOMED_USERS = set()
