54e47e0e4b8882f3a100af39f4660e8553d98652
evgeniyalexandr0vich <1511jeck@gmail.com>
2025-12-01 11:03:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ else:
 
 # Создаем папку
 os.makedirs(EXCEL_DIR, exist_ok=True)
-EXCEL_FILE = os.path.join(EXCEL_DIR, "work_tracker.xlsx")
+EXCEL_FILE = os.path.join(EXCEL_DIR, "Учет рабочего времени.xlsx")
 
 DEFAULT_REMINDER_HOUR = 18
 DEFAULT_REMINDER_MINUTE = 0
